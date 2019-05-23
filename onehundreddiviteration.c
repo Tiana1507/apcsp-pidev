fb8edbe6abf8569faa2dc91e569c74b69a0b7632
Tiana1507 <andrew.zi.tian@gmail.com>
2019-05-22 20:45:35 -0700

    Create onehundreddiviteration.c
    5/22 - HW

---
+++ onehundreddiviteration.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+        int j;
+        div = 13
+        for (int i - 0; i < 100; i++)
+        {
+                float j = i % div;
+                if (j == 0)
+                {
+                        printf("%d is divisible by %d", i, div);
+                }
+                else
+                {
+                        printf("%d is not divisible by %d", i, div);
+                }
+        }
+}
+
